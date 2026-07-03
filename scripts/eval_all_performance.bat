b01837b9eb5377c493395e604fc22485081aaff7
Felix Back <backfelix8@gmail.com>
2026-07-03 16:20:40 +0200

    add performance all bash script

---
+++ scripts/eval_all_performance.bat
@@ -0,0 +1,17 @@
+setlocal
+
+set SCRIPT=evaluate.py
+set OUTFILE=evaluate_all_performance.out
+
+echo Starte Jobs... > "%OUTFILE%"
+
+py -3.12 -u %SCRIPT% gbrt_standard performance >> "%OUTFILE%" 2>&1
+py -3.12 -u %SCRIPT% rf_standard performance >> "%OUTFILE%" 2>&1
+py -3.12 -u %SCRIPT% ffn_discrete performance >> "%OUTFILE%" 2>&1
+py -3.12 -u %SCRIPT% ffn_l2 performance >> "%OUTFILE%" 2>&1
+py -3.12 -u %SCRIPT% gbrt_standard_l2 performance >> "%OUTFILE%" 2>&1
+py -3.12 -u %SCRIPT% rf_standard_l2 performance >> "%OUTFILE%" 2>&1
+py -3.12 -u %SCRIPT% ffn_noise performance >> "%OUTFILE%" 2>&1
+
+
+echo Fertig! >> "%OUTFILE%"
